6a3d610f2ce73dcd8af2d9273e2eb60f7c8fa911
Moisés Pereira <moiseseletronica@gmail.com>
2022-10-06 22:41:36 -0300

    first commit

---
+++ src/main/java/com/calculadoradasorte/calculadoradasorte/CalculadoradasorteApplication.java
@@ -0,0 +1,13 @@
+package com.calculadoradasorte.calculadoradasorte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoradasorteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoradasorteApplication.class, args);
+	}
+
+}
